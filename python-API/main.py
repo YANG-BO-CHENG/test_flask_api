4f149d8f04a06411346a5667ab962b2119c43b49
YANG-BO-CHENG <55633233+YANG-BO-CHENG@users.noreply.github.com>
2024-01-12 17:16:31 +0800

    Add files via upload

---
+++ python-API/main.py
@@ -0,0 +1,42 @@
+from flask import Flask,request
+from flask_restful import Resource, Api
+
+user_list = []
+class User(Resource):
+     
+    def get(self, username):
+        for user in user_list:
+            if user['username'] == username:
+                return user
+        return {'username': None}, 404
+    def post(self, username):
+        user = {'username': username,'email': request.get_json().get('email')}
+        user_list.append(user)
+        return user
+    def delete(self, username):
+        for ind, user in enumerate(user_list):
+            if user['username'] == username:
+                deleted_user = user_list.pop(ind)
+                print(deleted_user)
+                return {'note': 'successfully delete'}
+    def put(self, username):
+        user_find = None
+        for user in user_list:
+            if user['username'] == username:
+                user_find = user
+        if user_find:
+            user_list.remove(user_find)
+            user_find['email'] = request.get_json().get('email')
+            user_list.append(user_find)
+            return user_find
+class UserList(Resource):
+    """會員列表 """
+    def get(self):
+        return {'user_list': user_list}   
+app = Flask(__name__)
+api = Api(app)
+api.add_resource(User, '/user/<string:username>')
+api.add_resource(UserList, '/users')
+
+if __name__ == '__main__':
+    app.run()
